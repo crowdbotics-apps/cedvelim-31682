59ce9a7d3cffdbd3fc6326c1e147570a79367850
Crowdbotics <team@crowdbotics.com>
2021-10-25 08:29:46 +0000

    Update models

--- backend/cedvelim_31682/wsgi.py
+++ backend/cedvelim_31682/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cedvelim_31682.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cedvelim_31682.settings")
 
 application = get_wsgi_application()
